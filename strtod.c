e6fee41bc6f8041f82898d863b0d05d3d2b542f7
Serena <39934557+ctrinh01028485@users.noreply.github.com>
2022-07-18 22:53:01 +0700

    Create strtod.c

---
+++ strtod.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+
+int test_strtod(char *string, char **ptr){
+    int result = 0, j = 0;
+    char *newPtr = (char *) malloc(20);
+    for (int i = 0; string[i] != '\0'; i++){
+        if (string[i] >= '0' && string[i] <= '9')
+            result = result*10 + (string[i] - 48);
+        else
+            newPtr[j++] = string[i];
+    }
+    newPtr[j] = '\0';
+    free(*ptr);
+    *ptr = newPtr;
+    return result;
+}
+
+
+int main(){
+    int val;
+    double vall;
+    char str[20];
+    char *ptr;
+    strcpy(str, "heohello123");
+    val = test_strtod(str, &ptr);
+    printf("chuoi: %s\n", ptr);
+    printf("val = %d", val);
+    free(ptr);
+    return 0;
+}
