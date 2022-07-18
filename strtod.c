145c9f546e0dc7a9c4956578b54cafa10ab087c0
Serena <39934557+ctrinh01028485@users.noreply.github.com>
2022-07-18 23:52:24 +0700

    Update strtod.c

--- strtod.c
+++ strtod.c
@@ -3,20 +3,34 @@
 #include <stdint.h>
 #include <string.h>
 
-int test_strtod(char *string, char **ptr){
-    int result = 0, j = 0;
+double test_strtod(char *string, char **ptr){
+    int j = 0;
+    double result = 0, count = 0;
     char *newPtr = (char *) malloc(20);
     for (int i = 0; string[i] != '\0'; i++){
-        if (string[i] >= '0' && string[i] <= '9')
-            result = result*10 + (string[i] - 48);
-        else
-            newPtr[j++] = string[i];
+        if (string[i] >= '0' && string[i] <= '9' && !result){
+            while ((string[i] >= '0' && string[i] <= '9') || string[i] == '.'){
+                if (string[i] == '.'){
+                    count++; i++;
+                    continue;
+                }
+                if (count){
+                    count *= 10;
+                    result = result + (double)(string[i] - 48)/count;
+                } else 
+                    result = result*10 + (string[i] - 48);
+                i++;
+            }
+            i--;
+        }
+        else newPtr[j++] = string[i];
     }
     newPtr[j] = '\0';
     free(*ptr);
     *ptr = newPtr;
     return result;
 }
+
 
 
 int main(){
