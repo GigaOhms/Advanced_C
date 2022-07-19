ceb436d7524ab5a9788346be19a065988e9264c6
Serena <39934557+ctrinh01028485@users.noreply.github.com>
2022-07-19 16:35:59 +0700

    add strcspn.c

---
+++ Bai2/strcspn.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int test_strcspn(char *a, char *b){
+    int i = 0, min = -1;
+    while (*b){
+        for (i = 0; *(a+i) != '\0'; i++){
+			printf("%c = %c; i = %d\n", *b, *(a+i), i);
+            if (*b == *(a + i) && (min > i || min == -1) ){
+                min = i;
+				printf("min = %d\n", min);
+			}
+		}
+		b++;
+	}
+    if (min == -1) return i;
+    return min;
+}
+
+int main(void) {
+	char a[] = "ABCDEF4960910";
+	char b[] = "M";
+	int len = test_strcspn(a, b);
+	printf("%d\n", len);
+    return 0;
+}
