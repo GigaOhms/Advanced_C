d5de85204774dc2f065193e42bdca8f96cbbf142
ctrinh01028485 <>
2022-07-22 09:43:39 +0000

    add strtok2

--- Bai2_string/strtok2.c
+++ Bai2_string/strtok2.c
@@ -1,31 +1,44 @@
 #include <stdio.h>
 
-// char *strtok_t(char *s, const char *delim){
-// 	static char *old;
-// 	if (s != NULL)
-// 		*old = s;
-// 	if (!(*old))
-// 		return 0;
-// 	for (int i = 0; 1; i++){
-// 		for ()
-// 	}
-	
-// }
-
-void ccc(char *s){
-	static char *old;
-	if (s != NULL)
-		old = s;
-	printf("%s\n", old);
+int strcspn_t(const char *s1, const char *s2) {
+	const char *s = s1;
+	const char *c;
+	while (*s1) {
+		for (c = s2; *c; c++)
+			if (*s1 == *c)
+				break;
+		if (*c)
+			break;
+		s1++;
+	}
+	return s1 - s;
 }
 
-int main(){
-    char arr[] = "We-are-learning-aboat-libray-stdlib";
-    // char *token = strtok_t(arr, "-");
-    // while (token != NULL){
-    //     printf("%s\n", token);
-    //     token = strtok_t(NULL, "-");
-    // }
-	ccc(arr);
-    return 0;
+
+char *strtok_t(char *s, const char *delim) {
+	static char *old;
+	if (s == NULL)
+		s = old;
+	if (*s == 0) {
+		old = s;
+		return NULL;
+	}
+	char *end = s + strcspn_t(s, delim);
+	if (*end == 0) {
+		old = end;
+		return s;
+	}
+	*end = 0;
+	old = end + 1;
+	return s;
+}
+
+int main() {
+	char arr[] = "We-are-learning-abffffoat-libray-stdlib";
+	char *token = strtok_t(arr, "-");
+	while (token != NULL) {
+		printf("%s\n", token);
+		token = strtok_t(NULL, "-");
+	}
+	return 0;
 }
